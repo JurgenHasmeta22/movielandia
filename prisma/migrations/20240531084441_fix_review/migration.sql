5625d122e61ede341d0110b90796f52c205a93dd
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-06-14 13:24:32 +0200

    many config stuff

---
+++ prisma/migrations/20240531084441_fix_review/migration.sql
@@ -0,0 +1,30 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_SerieReview" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "content" TEXT NOT NULL DEFAULT '',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME,
+    "userId" INTEGER NOT NULL,
+    "serieId" INTEGER NOT NULL,
+    CONSTRAINT "SerieReview_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "SerieReview_serieId_fkey" FOREIGN KEY ("serieId") REFERENCES "Serie" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_SerieReview" ("content", "createdAt", "id", "serieId", "updatedAt", "userId") SELECT "content", "createdAt", "id", "serieId", "updatedAt", "userId" FROM "SerieReview";
+DROP TABLE "SerieReview";
+ALTER TABLE "new_SerieReview" RENAME TO "SerieReview";
+CREATE TABLE "new_MovieReview" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "content" TEXT NOT NULL DEFAULT '',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME,
+    "userId" INTEGER NOT NULL,
+    "movieId" INTEGER NOT NULL,
+    CONSTRAINT "MovieReview_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "MovieReview_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_MovieReview" ("content", "createdAt", "id", "movieId", "updatedAt", "userId") SELECT "content", "createdAt", "id", "movieId", "updatedAt", "userId" FROM "MovieReview";
+DROP TABLE "MovieReview";
+ALTER TABLE "new_MovieReview" RENAME TO "MovieReview";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
