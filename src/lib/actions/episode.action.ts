48dd54446e53caca813894956089ad7ce8141d59
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-06-15 14:05:14 +0200

    prisma fix

--- src/lib/actions/episode.action.ts
+++ src/lib/actions/episode.action.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { Episode, Prisma } from "@prisma/client";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 interface EpisodeModelParams {
     sortBy: string;
