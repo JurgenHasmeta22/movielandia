29c7e8c5542b52b8885755122fa94ae942db3e6e
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-06-15 14:49:59 +0200

    genre individual finished

--- src/components/paginationControl/PaginationControl.tsx
+++ src/components/paginationControl/PaginationControl.tsx
@@ -7,15 +7,24 @@ import { useSearchParams, useRouter } from "next/navigation";
 interface IPaginationControl {
     pageCount: number;
     currentPage: number;
+    dataType?: string;
 }
 
-const PaginationControl: React.FC<IPaginationControl> = ({ pageCount, currentPage }) => {
+const PaginationControl: React.FC<IPaginationControl> = ({ pageCount, currentPage, dataType }) => {
     const searchParams = useSearchParams();
     const router = useRouter();
 
     const onPageChange = (event: React.ChangeEvent<unknown>, value: number) => {
         const newSearchParams = new URLSearchParams(searchParams.toString());
-        newSearchParams.set("page", String(value));
+
+        if (dataType === "Movies") {
+            newSearchParams.set("pageMovies", String(value));
+        } else if (dataType === "Series") {
+            newSearchParams.set("pageSeries", String(value));
+        } else {
+            newSearchParams.set("page", String(value));
+        }
+
         router.push(`?${newSearchParams.toString()}`);
     };
 
