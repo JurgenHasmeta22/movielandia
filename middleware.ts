c541a0e16ed619a3ec8bb56198b35051b92f85c2
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-06-16 17:58:51 +0200

    middleware bug redirect

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,22 @@
-export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(async function middleware(req) {
+    const token = await getToken({ req });
+    const isAuthenticated = !!token;
+
+    if (req.nextUrl.pathname.startsWith("/login")) {
+        if (isAuthenticated) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    } else if (req.nextUrl.pathname.startsWith("/register")) {
+        if (isAuthenticated) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    }
+});
 
 export const config = {
-    matcher: ["/admin/:path*", "/profile"],
+    matcher: ["/", "/login", "/register", "/profile"],
 };
