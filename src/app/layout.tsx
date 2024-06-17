23cd944b151cad9b3084b196cf4549b8a03e15b7
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-06-17 12:16:09 +0200

    theme fix script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,34 @@
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import { Grid } from "@mui/material";
 import ScrollToTop from "@/components/scrollToTop/ScrollToTop";
-import Header from "@/components/header/Header";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import ToastProvider from "@/lib/toastify/ToastProvider";
 import { CustomThemeProvider } from "@/utils/theme";
 import { Providers } from "./providers";
 import "./globals.css";
-import ToastProvider from "@/lib/toastify/ToastProvider";
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const setInitialTheme = `
+        (function() {
+            const theme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+            if (theme === 'dark') {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+        })();
+    `;
+
     return (
         <html lang="en">
+            <head>
+                <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
+            </head>
             <body>
                 <Providers>
                     <AppRouterCacheProvider>
