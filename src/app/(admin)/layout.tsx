749cf1f09d5622ecb84511cad9981accd6cb069d
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-07-27 15:22:26 +0200

    notfound bug remains href

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,44 +1,29 @@
 import { Grid } from "@mui/material";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import ToastProvider from "@/providers/ToastProvider";
-import { CustomThemeProvider } from "@/utils/theme/theme";
-import { ModalProvider } from "@/providers/ModalProvider";
-import { RightPanelProvider } from "@/providers/RightPanelProvider";
-import { AuthProvider } from "../../providers/AuthProvider";
 import Sidebar from "@/components/admin/layout/sidebar/Sidebar";
 import TopBar from "@/components/admin/layout/topBar/TopBar";
 import { SidebarItems } from "@/utils/componentHelpers/SidebarItems";
 
-export default function RootLayout({
+export const metadata = {
+    robots: {
+        follow: false,
+        index: false,
+    },
+};
+
+export default function AdminLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body>
-                <AuthProvider>
-                    <AppRouterCacheProvider>
-                        <CustomThemeProvider>
-                            <ToastProvider>
-                                <ModalProvider>
-                                    <RightPanelProvider>
-                                        <Grid container component={"main"}>
-                                            <Grid item xs={12} md={2}>
-                                                <Sidebar sidebarItems={SidebarItems} />
-                                            </Grid>
-                                            <Grid item xs={12} md={10}>
-                                                <TopBar />
-                                                <main style={{ marginLeft: 3 }}>{children}</main>
-                                            </Grid>
-                                        </Grid>
-                                    </RightPanelProvider>
-                                </ModalProvider>
-                            </ToastProvider>
-                        </CustomThemeProvider>
-                    </AppRouterCacheProvider>
-                </AuthProvider>
-            </body>
-        </html>
+        <Grid container component={"main"}>
+            <Grid item xs={12} md={2}>
+                <Sidebar sidebarItems={SidebarItems} />
+            </Grid>
+            <Grid item xs={12} md={10}>
+                <TopBar />
+                <main style={{ marginLeft: 3 }}>{children}</main>
+            </Grid>
+        </Grid>
     );
 }
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,14 @@
+import { MetadataRoute } from "next";
+
 const baseUrl = process.env.NEXT_PUBLIC_PROJECT_URL;
 
-export default function robots() {
+export default function robots(): MetadataRoute.Robots {
     return {
         rules: [
             {
                 userAgent: "*",
+                allow: "/",
+                disallow: ["/admin/"],
             },
         ],
         sitemap: `${baseUrl}/sitemap.xml`,
