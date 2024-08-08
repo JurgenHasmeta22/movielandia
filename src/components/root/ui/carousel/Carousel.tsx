f9ef702346ccfbd74f6959eaac1b5cc78a4034cc
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-08-08 15:51:22 +0200

    image fix carousel and data.ts fix

--- src/components/root/ui/carousel/Carousel.tsx
+++ src/components/root/ui/carousel/Carousel.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Slider from "react-slick";
-import { Box, Button, Typography, IconButton, useTheme } from "@mui/material";
+import { Box, Button, Typography, IconButton, useTheme, useMediaQuery } from "@mui/material";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import Link from "next/link";
@@ -67,6 +67,7 @@ const CustomPrevArrow = (props: any) => {
 const Carousel = ({ data, type }: ICarouselProps) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
+    const isMobile = useMediaQuery("(max-width:768px)");
 
     const settings = {
         dots: false,
@@ -81,58 +82,83 @@ const Carousel = ({ data, type }: ICarouselProps) => {
     };
 
     return (
-        <Box sx={{ position: "relative", height: "95vh", overflow: "hidden" }}>
+        <Box sx={{ position: "relative", height: isMobile ? "130vh" : "90vh", overflow: "hidden" }} mt={4}>
             <Slider {...settings}>
                 {data.map((element, index) => (
-                    <Box key={index} sx={{ position: "relative", height: "95vh" }}>
-                        <Image
-                            src={element.photoSrcProd}
-                            alt={`Slide ${index}`}
-                            fill
-                            quality={100}
-                            priority
-                            style={{ objectPosition: "center", objectFit: "fill" }}
-                        />
+                    <Box
+                        key={index}
+                        sx={{
+                            position: "relative",
+                            height: isMobile ? "130vh" : "90vh",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            p: 2,
+                        }}
+                    >
                         <Box
                             sx={{
-                                position: "absolute",
-                                bottom: 0,
-                                left: 0,
-                                width: "100%",
-                                height: "40%",
-                                backgroundImage: "linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))",
-                                color: "white",
                                 display: "flex",
                                 flexDirection: "column",
-                                justifyContent: "flex-end",
-                                p: 4,
+                                alignItems: "center",
+                                justifyContent: "center",
+                                width: "auto",
+                                maxWidth: "100%",
                             }}
                         >
-                            <Typography variant="h1" gutterBottom>
-                                {element.title}
-                            </Typography>
-                            <Typography variant="body1" sx={{ mb: 2 }}>
-                                {element.description}
-                            </Typography>
-                            <Box>
-                                <Link href={`/${type}/${element.id}`} passHref>
-                                    <Button
-                                        variant="contained"
-                                        color="secondary"
-                                        sx={{
-                                            bgcolor: colors.redAccent[500],
-                                            "&:hover": { bgcolor: colors.greenAccent[500], color: colors.primary[100] },
-                                            textTransform: "capitalize",
-                                            color: colors.grey[900],
-                                            fontSize: 16,
-                                            fontWeight: 700,
-                                            px: 4,
-                                            py: 1,
-                                        }}
-                                    >
-                                        See Details
-                                    </Button>
-                                </Link>
+                            <Image
+                                src={element.photoSrcProd}
+                                alt={`Slide ${index}`}
+                                quality={100}
+                                height={320}
+                                width={220}
+                                priority
+                                style={{ objectFit: "contain", objectPosition: "center" }}
+                            />
+                            <Box
+                                sx={{
+                                    position: "absolute",
+                                    bottom: 0,
+                                    left: 0,
+                                    width: "100%",
+                                    height: "40%",
+                                    backgroundImage: "linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))",
+                                    color: "white",
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    justifyContent: "flex-end",
+                                    p: 4,
+                                }}
+                            >
+                                <Typography variant="h1" gutterBottom>
+                                    {element.title}
+                                </Typography>
+                                <Typography variant="body1" sx={{ mb: 2 }}>
+                                    {element.description}
+                                </Typography>
+                                <Box>
+                                    <Link href={`/${type}/${element.id}`} passHref>
+                                        <Button
+                                            variant="contained"
+                                            color="secondary"
+                                            sx={{
+                                                bgcolor: colors.redAccent[500],
+                                                "&:hover": {
+                                                    bgcolor: colors.greenAccent[500],
+                                                    color: colors.primary[100],
+                                                },
+                                                textTransform: "capitalize",
+                                                color: colors.grey[900],
+                                                fontSize: 16,
+                                                fontWeight: 700,
+                                                px: 4,
+                                                py: 1,
+                                            }}
+                                        >
+                                            See Details
+                                        </Button>
+                                    </Link>
+                                </Box>
                             </Box>
                         </Box>
                     </Box>
