62d617cace473d56546388fd41c6eef92a27225d
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-08-17 23:38:55 +0200

    trying to do the react-email with resend but have problems

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -1,14 +1,19 @@
-"use server";
-
 import { hashSync } from "bcrypt";
 import { User } from "@prisma/client";
 import { prisma } from "../../prisma/config/prisma";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { redirect } from "next/navigation";
+import { randomUUID } from "crypto";
+import { Resend } from "resend";
+import RegistrationEmail from "../../emails/RegistrationEmail";
 
 interface IRegister {
     email: string;
     password: string;
     userName: string;
 }
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function signUp(userData: IRegister): Promise<User | null | undefined> {
     try {
@@ -19,31 +24,39 @@ export async function signUp(userData: IRegister): Promise<User | null | undefin
         });
 
         if (existingUser) {
-            return null;
+            throw new Error("User already exists.");
         } else {
             const hash = hashSync(password, 7);
 
             const user: User | null = await prisma.user.create({
-                data: { email, password: hash, userName },
-                include: {
-                    favMovies: { include: { movie: true } },
-                    favSeries: { include: { serie: true } },
-                    movieReviews: { include: { movie: true } },
-                    serieReviews: { include: { serie: true } },
-                    movieReviewsUpvoted: { include: { movieReview: true, movie: true } },
-                    movieReviewsDownvoted: { include: { movieReview: true, movie: true } },
-                    serieReviewsUpvoted: { include: { serieReview: true, serie: true } },
-                    serieReviewsDownvoted: { include: { serieReview: true, serie: true } },
+                data: { email, password: hash, userName, active: false },
+            });
+
+            const token = await prisma.activateToken.create({
+                data: {
+                    userId: user.id,
+                    token: `${randomUUID()}${randomUUID()}`.replace(/-/g, ""),
                 },
             });
 
-            if (user) {
-                return user;
+            if (user && token) {
+                await resend.emails.send({
+                    from: "MovieLandia24 <onboarding@resend.dev>",
+                    to: [email],
+                    subject: "Registration Verification - Movielandia24",
+                    react: RegistrationEmail({ userName, email, token: token.token }),
+                });
+
+                redirect("/login");
             } else {
-                return null;
+                throw new Error("Failed to register user.");
             }
         }
     } catch (error) {
-        console.error(error);
+        if (isRedirectError(error)) {
+            throw error;
+        } else {
+            throw new Error(error instanceof Error ? error.message : "An unexpected error occurred.");
+        }
     }
 }
