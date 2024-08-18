e18d150b009a3d834168da2f081891075b17e9fa
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-08-18 12:29:36 +0200

    finished the verification email feature

--- src/app/(root)/verification-sent/page.tsx
+++ src/app/(root)/verification-sent/page.tsx
@@ -16,27 +16,45 @@ export default function VerifyPage() {
                 alignItems: "center",
             }}
         >
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    p: 4,
-                    boxShadow: 6,
-                    borderRadius: 4,
-                    backgroundColor: "background.paper",
-                }}
-            >
-                <Typography variant="h4" component="h1" gutterBottom>
-                    Verify Your Email
-                </Typography>
-                <Typography variant="body1" sx={{ mb: 3 }}>
-                    We have sent a verification link to <strong>{email}</strong>.
-                </Typography>
-                <Typography variant="body2">
-                    Please check your email and follow the instructions to verify your account.
-                </Typography>
-            </Box>
+            {email && email.length > 0 ? (
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        p: 4,
+                        boxShadow: 6,
+                        borderRadius: 4,
+                        backgroundColor: "background.paper",
+                    }}
+                >
+                    <Typography variant="h4" component="h1" gutterBottom>
+                        Verify Your Email
+                    </Typography>
+                    <Typography variant="body1" sx={{ mb: 3 }}>
+                        We have sent a verification link to <strong>{email}</strong>.
+                    </Typography>
+                    <Typography variant="body2">
+                        Please check your email and follow the instructions to verify your account.
+                    </Typography>
+                </Box>
+            ) : (
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        p: 4,
+                        boxShadow: 6,
+                        borderRadius: 4,
+                        backgroundColor: "background.paper",
+                    }}
+                >
+                    <Typography variant="h4" component="h1" gutterBottom>
+                        Email not provided
+                    </Typography>
+                </Box>
+            )}
         </Container>
     );
 }
