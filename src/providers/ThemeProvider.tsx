e82e39ffddb9d1d6bf3da4b41c775f55e5bd0fe8
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-08-27 17:48:00 +0200

    MUI v6 stable release upgrade started

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,16 +1,16 @@
 import React, { ReactNode } from "react";
-import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import CssBaseline from "@mui/material/CssBaseline";
 import { theme } from "@/utils/theme/theme";
 
-export function ThemeProvider({ children }: { children: ReactNode }) {
+export function MUIThemeProvider({ children }: { children: ReactNode }) {
     return (
         <AppRouterCacheProvider>
-            <CssVarsProvider defaultMode="dark" theme={theme}>
+            <ThemeProvider theme={theme} disableTransitionOnChange>
                 <CssBaseline />
                 {children}
-            </CssVarsProvider>
+            </ThemeProvider>
         </AppRouterCacheProvider>
     );
 }
