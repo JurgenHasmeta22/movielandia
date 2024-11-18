bb758d142eaf78e5cd3958710f2182e2a974c2f6
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-11-18 15:57:59 +0100

    Removed some favicon not needed

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -19,17 +19,6 @@ export default function AdminLayout({ children }: Readonly<IAdminLayoutProps>) {
         <>
             <Head>
                 <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon/favicon-16x16.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon/favicon-96x96.png" />
-                <link rel="icon" type="image/png" sizes="192x192" href="/icons/favicon/favicon-192x192.png" />
-                <link rel="apple-touch-icon" sizes="57x57" href="/icons/favicon/favicon-57x57.png" />
-                <link rel="apple-touch-icon" sizes="60x60" href="/icons/favicon/favicon-60x60.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/icons/favicon/favicon-72x72.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/icons/favicon/favicon-76x76.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/icons/favicon/favicon-114x114.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/icons/favicon/favicon-120x120.png" />
-                <link rel="apple-touch-icon" sizes="144x144" href="/icons/favicon/favicon-144x144.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/icons/favicon/favicon-152x152.png" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/icons/favicon/favicon-180x180.png" />
             </Head>
             <Grid container component={"main"}>
