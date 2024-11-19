f6d471058e0ba02b549e4f30a5c12e649c10761b
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-11-19 17:29:33 +0100

    Add type prop to MuiNextLink for main logo styling, also updated the mainLogo

--- src/components/root/muiNextLink/MuiNextLink.tsx
+++ src/components/root/muiNextLink/MuiNextLink.tsx
@@ -5,10 +5,25 @@ type MuiLinkProps = Parameters<typeof MuiLink>[0] & {
     href: string;
     prefetch?: boolean;
     ref: any;
+    type?: string;
 };
 
 const MuiNextLink = (props: MuiLinkProps) => {
-    return <MuiLink component={NextLink} {...props}></MuiLink>;
+    return (
+        <MuiLink
+            component={NextLink}
+            {...props}
+            sx={{
+                ":hover":
+                    props.type && props.type === "mainLogo"
+                        ? {
+                              backgroundColor: "transparent",
+                              color: "inherit",
+                          }
+                        : {},
+            }}
+        ></MuiLink>
+    );
 };
 
 export default MuiNextLink;
