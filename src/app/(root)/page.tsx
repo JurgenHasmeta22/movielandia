57a493152a13262b5413d42020338efb6e7cdb4d
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-12-04 15:23:03 +0100

    revert to server actions test

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,6 +7,8 @@ import HomeHeroSection from "./(home)/_components/HomeHero";
 import ListHomeSection from "./(home)/_components/ListHomeSection";
 import MarketingSection from "./(home)/_components/MarketingSection";
 import NewsletterSection from "./(home)/_components/NewsletterSection";
+import { getMoviesWithFilters } from "@/actions/movie.actions";
+import { getSeriesWithFilters } from "@/actions/serie.actions";
 
 export const metadata: Metadata = {
     title: "MovieLandia24 - Your Ultimate Destination for Movies",
@@ -41,18 +43,25 @@ export default async function Home() {
         page: 1,
     };
 
-    const moviesResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_PROJECT_URL}/api/movies?page=${queryParams.page}${session?.user?.id ? `&userId=${session.user.id}` : ""}`,
-        { cache: "no-store" },
-    );
-    const moviesData = await moviesResponse.json();
+    // API Route approach:
+    // const moviesResponse = await fetch(
+    //     `${process.env.NEXT_PUBLIC_PROJECT_URL}/api/movies?page=${queryParams.page}${session?.user?.id ? `&userId=${session.user.id}` : ""}`,
+    //     { cache: "no-store" },
+    // );
+    // const moviesData = await moviesResponse.json();
+    // const movies: Movie[] = moviesData.movies;
+
+    // const seriesResponse = await fetch(
+    //     `${process.env.NEXT_PUBLIC_PROJECT_URL}/api/series?page=${queryParams.page}${session?.user?.id ? `&userId=${session.user.id}` : ""}`,
+    //     { cache: "no-store" },
+    // );
+    // const seriesData = await seriesResponse.json();
+    // const series: Serie[] = seriesData.rows;
+
+    const moviesData = await getMoviesWithFilters(queryParams, Number(session?.user?.id));
     const movies: Movie[] = moviesData.movies;
 
-    const seriesResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_PROJECT_URL}/api/series?page=${queryParams.page}${session?.user?.id ? `&userId=${session.user.id}` : ""}`,
-        { cache: "no-store" },
-    );
-    const seriesData = await seriesResponse.json();
+    const seriesData = await getSeriesWithFilters(queryParams, Number(session?.user?.id));
     const series: Serie[] = seriesData.rows;
 
     return (
