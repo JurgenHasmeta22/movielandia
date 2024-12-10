4d4ecc7280553217c8459aca31e5b8cc716e483b
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-12-10 20:54:17 +0100

    added some api routes for search autocomplete

---
+++ src/app/api/search/actors/route.ts
@@ -0,0 +1,87 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../../prisma/config/prisma";
+import { Actor } from "@prisma/client";
+
+type RatingsMap = {
+    [key: number]: {
+        averageRating: number;
+        totalReviews: number;
+    };
+};
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+    try {
+        const searchParams = request.nextUrl.searchParams;
+        const fullname = searchParams.get("fullname") || "";
+        const page = Number(searchParams.get("page")) || 1;
+        const sortBy = searchParams.get("sortBy") || undefined;
+        const ascOrDesc = searchParams.get("ascOrDesc") as "asc" | "desc" | undefined;
+        const userId = searchParams.get("userId") ? Number(searchParams.get("userId")) : undefined;
+
+        const orderByObject: any = {};
+        if (sortBy && ascOrDesc) {
+            orderByObject[sortBy] = ascOrDesc;
+        }
+
+        const query = {
+            where: {
+                fullname: { contains: fullname },
+            },
+            orderBy: orderByObject,
+            skip: (page - 1) * 12,
+            take: 12,
+        };
+
+        const actors = await prisma.actor.findMany(query);
+        const actorIds = actors.map((actor: Actor) => actor.id);
+
+        const actorRatings = await prisma.actorReview.groupBy({
+            by: ["actorId"],
+            where: { actorId: { in: actorIds } },
+            _avg: {
+                rating: true,
+            },
+            _count: {
+                rating: true,
+            },
+        });
+
+        const actorRatingsMap: RatingsMap = actorRatings.reduce((map, rating) => {
+            map[rating.actorId] = {
+                averageRating: rating._avg.rating || 0,
+                totalReviews: rating._count.rating,
+            };
+            return map;
+        }, {} as RatingsMap);
+
+        const actorsFinal = await Promise.all(
+            actors.map(async (actor) => {
+                const { ...properties } = actor;
+                let isBookmarked = false;
+
+                if (userId) {
+                    const existingFavorite = await prisma.userActorFavorite.findFirst({
+                        where: {
+                            AND: [{ userId }, { actorId: actor.id }],
+                        },
+                    });
+                    isBookmarked = !!existingFavorite;
+                }
+
+                const ratingsInfo = actorRatingsMap[actor.id] || { averageRating: 0, totalReviews: 0 };
+                return { ...properties, ...ratingsInfo, ...(userId && { isBookmarked }) };
+            }),
+        );
+
+        const count = await prisma.actor.count({
+            where: {
+                fullname: { contains: fullname },
+            },
+        });
+
+        return NextResponse.json({ actors: actorsFinal, count });
+    } catch (error) {
+        console.error("Error searching actors:", error);
+        return NextResponse.json({ error: "Failed to search actors" }, { status: 500 });
+    }
+} 
---
+++ src/app/api/search/series/route.ts
@@ -0,0 +1,87 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../../prisma/config/prisma";
+import { Serie } from "@prisma/client";
+
+type RatingsMap = {
+    [key: number]: {
+        averageRating: number;
+        totalReviews: number;
+    };
+};
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+    try {
+        const searchParams = request.nextUrl.searchParams;
+        const title = searchParams.get("title") || "";
+        const page = Number(searchParams.get("page")) || 1;
+        const sortBy = searchParams.get("sortBy") || undefined;
+        const ascOrDesc = searchParams.get("ascOrDesc") as "asc" | "desc" | undefined;
+        const userId = searchParams.get("userId") ? Number(searchParams.get("userId")) : undefined;
+
+        const orderByObject: any = {};
+        if (sortBy && ascOrDesc) {
+            orderByObject[sortBy] = ascOrDesc;
+        }
+
+        const query = {
+            where: {
+                title: { contains: title },
+            },
+            orderBy: orderByObject,
+            skip: (page - 1) * 12,
+            take: 12,
+        };
+
+        const series = await prisma.serie.findMany(query);
+        const serieIds = series.map((serie) => serie.id);
+
+        const serieRatings = await prisma.serieReview.groupBy({
+            by: ["serieId"],
+            where: { serieId: { in: serieIds } },
+            _avg: {
+                rating: true,
+            },
+            _count: {
+                rating: true,
+            },
+        });
+
+        const serieRatingsMap: RatingsMap = serieRatings.reduce((map, rating) => {
+            map[rating.serieId] = {
+                averageRating: rating._avg.rating || 0,
+                totalReviews: rating._count.rating,
+            };
+            return map;
+        }, {} as RatingsMap);
+
+        const seriesFinal = await Promise.all(
+            series.map(async (serie) => {
+                const { ...properties } = serie;
+                let isBookmarked = false;
+
+                if (userId) {
+                    const existingFavorite = await prisma.userSerieFavorite.findFirst({
+                        where: {
+                            AND: [{ userId }, { serieId: serie.id }],
+                        },
+                    });
+                    isBookmarked = !!existingFavorite;
+                }
+
+                const ratingsInfo = serieRatingsMap[serie.id] || { averageRating: 0, totalReviews: 0 };
+                return { ...properties, ...ratingsInfo, ...(userId && { isBookmarked }) };
+            }),
+        );
+
+        const count = await prisma.serie.count({
+            where: {
+                title: { contains: title },
+            },
+        });
+
+        return NextResponse.json({ rows: seriesFinal, count });
+    } catch (error) {
+        console.error("Error searching series:", error);
+        return NextResponse.json({ error: "Failed to search series" }, { status: 500 });
+    }
+} 
