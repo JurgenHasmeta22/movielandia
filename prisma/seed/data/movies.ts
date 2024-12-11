62f6fe8c303ebfc05857cbcb31417cb62a7cf183
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-12-11 14:17:48 +0100

    cleaned up seed data

---
+++ prisma/seed/data/movies.ts
@@ -0,0 +1,244 @@
+export const movies = [
+    {
+        id: 1,
+        title: "Freaks Out",
+        trailerSrc: "https://www.youtube.com/embed/D8La5G1DzCM",
+        duration: 141,
+        dateAired: "10/28/2021",
+        ratingImdb: 7.4,
+        description:
+            "Matilde, Cencio, Fulvio, and Mario are united when World War II strikes Rome. Israel, their circus owner, disappears in an attempt to find a place abroad for all of them.",
+        photoSrc: "http://localhost:4000/images/movies/1TkkTo8UiRl5lWM5qkAISHXg0fU.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/1TkkTo8UiRl5lWM5qkAISHXg0fU.jpg",
+    },
+    {
+        id: 2,
+        title: "My Father's Violin",
+        trailerSrc: "https://www.youtube.com/embed/GAmo87Ep-tI",
+        duration: 112,
+        dateAired: "11/17/2022",
+        ratingImdb: 6.5,
+        description:
+            "Through their shared grief and connection to music, an orphaned girl bonds with her emotionally distant, successful violinist uncle.",
+        photoSrc: "http://localhost:4000/images/movies/fathersviolin.jpg",
+        photoSrcProd: "https://movielandia-avenger22s-projects.vercel.app/images/movies/fathersviolin.jpg",
+    },
+    {
+        id: 3,
+        title: "The Invisible Guest",
+        trailerSrc: "https://www.youtube.com/embed/epCg2RbyF80",
+        duration: 126,
+        dateAired: "03/24/2017",
+        ratingImdb: 8.1,
+        description:
+            "A young businessman faces a lawyer in an attempt to prove his innocence for the murder of his girlfriend.",
+        photoSrc: "http://localhost:4000/images/movies/8c9fce3c0ffa46576423d44b525447edc25f1396.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/8c9fce3c0ffa46576423d44b525447edc25f1396.jpg",
+    },
+    {
+        id: 4,
+        title: "Blood and Bone",
+        trailerSrc: "https://www.youtube.com/embed/KyOtQr5u-uY",
+        duration: 93,
+        dateAired: "01/17/2009",
+        ratingImdb: 6.7,
+        description:
+            "Ex-con Boun is the new force on the underground street fighting scene in Los Angeles. When he defeats the reigning champion, the local gangster boss wants to enlist Boun in a series of high-stakes international matches. Boun's refusal sets off an explosive showdown between two powerful enemies risking everything.",
+        photoSrc: "http://localhost:4000/images/movies/5254_1_ladsfgfdgdfgdfgfdgfdgrge.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/5254_1_ladsfgfdgdfgdfgfdgfdgrge.jpg",
+    },
+    {
+        id: 5,
+        title: "Spider-Man: No Way Home",
+        trailerSrc: "https://www.youtube.com/embed/JfVOs4VSpmA",
+        duration: 148,
+        dateAired: "12/17/2021",
+        ratingImdb: 8.7,
+        description:
+            "Peter Parker is unmasked and no longer able to separate his normal life from the high stakes of being a Super Hero. When he seeks help from Doctor Strange, the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+        photoSrc: "http://localhost:4000/images/movies/1gxZrx9gL9ov2c1NpXimEUzMTmh.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/1gxZrx9gL9ov2c1NpXimEUzMTmh.jpg",
+    },
+    {
+        id: 6,
+        title: "Iron Will",
+        trailerSrc: "https://www.youtube.com/embed/giLeHfkJeXU",
+        duration: 108,
+        dateAired: "01/17/1994",
+        ratingImdb: 6.6,
+        description:
+            "After his father's death, the responsibility to care for the family falls on Will's shoulders. He enters a dog race that requires long hours of racing and a steely will to win.",
+        photoSrc: "http://localhost:4000/images/movies/6Ujbtp0NklUoQ67s32HyW6R5540.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/6Ujbtp0NklUoQ67s32HyW6R5540.jpg",
+    },
+    {
+        id: 7,
+        title: "Panama",
+        trailerSrc: "https://www.youtube.com/embed/2GSzkfe1aAY",
+        duration: 94,
+        dateAired: "11/18/2022",
+        ratingImdb: 0,
+        description:
+            "A former marine is hired by a defense contractor to travel to Panama to complete an arms deal. During the process, he becomes involved in the U.S. invasion of Panama and learns an important lesson about the true nature of political power.",
+        photoSrc: "http://localhost:4000/images/movies/82I3tDsGDTMy7lHar84Gz0jUuyW.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/82I3tDsGDTMy7lHar84Gz0jUuyW.jpg",
+    },
+    {
+        id: 8,
+        title: "Eternals",
+        trailerSrc: "https://www.youtube.com/embed/x_me3xsvDgk",
+        duration: 156,
+        dateAired: "11/05/2021",
+        ratingImdb: 6.8,
+        description:
+            "The Eternals are a team of ancient aliens who have been living on Earth in secret for thousands of years. When an unexpected tragedy forces them out of the shadows, they must reunite against mankind's oldest enemy.",
+        photoSrc: "http://localhost:4000/images/movies/1RjyfPLsZTq3lUdJY7pTzcmpPKl.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/1RjyfPLsZTq3lUdJY7pTzcmpPKl.jpg",
+    },
+    {
+        id: 9,
+        title: "Money Trap",
+        trailerSrc: "https://www.youtube.com/embed/gcH0KPQVVH4",
+        duration: 121,
+        dateAired: "12/29/2022",
+        ratingImdb: 6,
+        description:
+            "Asim Noyan deceives people with his lies and games. Asim Noyan and his gang, whom no one else has been able to capture, get involved in another scam.",
+        photoSrc: "http://localhost:4000/images/movies/4kiVg3QJQghjtRupyfWYI3T1R0O-1.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/4kiVg3QJQghjtRupyfWYI3T1R0O-1.jpg",
+    },
+    {
+        id: 10,
+        title: "Inch'Allah",
+        trailerSrc: "https://www.youtube.com/embed/53XLPquQD6Q",
+        duration: 102,
+        dateAired: "09/09/2012",
+        ratingImdb: 6.8,
+        description:
+            "A Canadian doctor finds her sympathies severely tested while working in the conflict-ravaged Palestinian territories.",
+        photoSrc: "http://localhost:4000/images/movies/9BCd5LdVHQmWqEnDTJ5Ut8idstP-1.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/9BCd5LdVHQmWqEnDTJ5Ut8idstP-1.jpg",
+    },
+    {
+        id: 11,
+        title: "Mobile Suit Gundam: Hathaway",
+        trailerSrc: "https://www.youtube.com/embed/MzxnPP5TLeU",
+        duration: 95,
+        dateAired: "06/11/2021",
+        ratingImdb: 6.6,
+        description:
+            "Hathaway Noa becomes part of a terrorist organization called Mufti that organizes an attack on Earth Federation ministers.",
+        photoSrc: "http://localhost:4000/images/movies/6gw8onh4FKsruBA7Oouv01EFxzn-1.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/6gw8onh4FKsruBA7Oouv01EFxzn-1.jpg",
+    },
+    {
+        id: 12,
+        title: "Rescued by Ruby",
+        trailerSrc: "https://www.youtube.com/embed/-Pwxr307O4w",
+        duration: 90,
+        dateAired: "03/17/2022",
+        ratingImdb: 7.3,
+        description:
+            "Pursuing his dream to join an elite K-9 unit, a state trooper partners with a smart but unruly shelter dog named Ruby.",
+        photoSrc: "http://localhost:4000/images/movies/ruby.jpg",
+        photoSrcProd: "https://movielandia-avenger22s-projects.vercel.app/images/movies/ruby.jpg",
+    },
+    {
+        id: 13,
+        title: "The Girl on the Mountain",
+        trailerSrc: "https://www.youtube.com/embed/Y0vh8jQ7HMQ",
+        duration: 94,
+        dateAired: "08/21/2021",
+        ratingImdb: 6.4,
+        description:
+            "When an isolated forester finds a mute little girl wandering alone in the woods, he must protect her from the evil forces determined to end her life.",
+        photoSrc: "http://localhost:4000/images/movies/1ZiZ3eVUWPxJROTkYbH8FBC9UuB.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/1ZiZ3eVUWPxJROTkYbH8FBC9UuB.jpg",
+    },
+    {
+        id: 14,
+        title: "The Weekend Away",
+        trailerSrc: "https://www.youtube.com/embed/XpfjN-7Hf-8",
+        duration: 89,
+        dateAired: "03/03/2022",
+        ratingImdb: 5.6,
+        description:
+            "A weekend getaway to Croatia goes awry when a woman is accused of murdering her best friend. As she attempts to clear her name and uncover the truth, her efforts reveal a painful secret.",
+        photoSrc: "http://localhost:4000/images/movies/6MS0QEl7UK2gdFFbHfNwuYlsq4H.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/6MS0QEl7UK2gdFFbHfNwuYlsq4H.jpg",
+    },
+    {
+        id: 15,
+        title: "Black Crab",
+        trailerSrc: "https://www.youtube.com/embed/fmjKsL_-rfw",
+        duration: 114,
+        dateAired: "03/18/2022",
+        ratingImdb: 5.7,
+        description:
+            "In a post-apocalyptic world, six soldiers on a covert mission must transport a mysterious package across a frozen archipelago.",
+        photoSrc: "http://localhost:4000/images/movies/blackcrab.jpg",
+        photoSrcProd: "https://movielandia-avenger22s-projects.vercel.app/images/movies/blackcrab.jpg",
+    },
+    {
+        id: 16,
+        title: "The Extra Man",
+        trailerSrc: "https://www.youtube.com/embed/etxKmplTT9Q",
+        duration: 108,
+        dateAired: "07/30/2010",
+        ratingImdb: 5.8,
+        description: "A man who escorts wealthy women in Manhattan takes a young man under his wing.",
+        photoSrc: "http://localhost:4000/images/movies/7RhiXoCm6yEflHQvYtoLeAkDhaU.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/7RhiXoCm6yEflHQvYtoLeAkDhaU.jpg",
+    },
+    {
+        id: 17,
+        title: "14 Peaks: Nothing Is Impossible",
+        trailerSrc: "https://www.youtube.com/embed/8QH5hBOoz08",
+        duration: 101,
+        dateAired: "12/09/2021",
+        ratingImdb: 7.8,
+        description:
+            "Fearless Nepali mountaineer Nirmal Purja embarks on a seemingly impossible quest to summit all 14 of the world's 8,000-meter peaks in seven months.",
+        photoSrc: "http://localhost:4000/images/movies/beyondpossible.jpg",
+        photoSrcProd: "https://movielandia-avenger22s-projects.vercel.app/images/movies/beyondpossible.jpg",
+    },
+    {
+        id: 18,
+        title: "The Innocents",
+        trailerSrc: "https://www.youtube.com/embed/T8HR-zTRrCM",
+        duration: 117,
+        dateAired: "06/26/2021",
+        ratingImdb: 7,
+        description:
+            "During the long days of the polar summer, a group of children reveal their dark and mysterious powers while the adults aren't watching. This playtime takes a dangerous turn.",
+        photoSrc: "http://localhost:4000/images/movies/4hIptu4Yre7pIUhFa7GBZlfDTPW.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/4hIptu4Yre7pIUhFa7GBZlfDTPW.jpg",
+    },
+    {
+        id: 19,
+        title: "Mean Dreams",
+        trailerSrc: "https://www.youtube.com/embed/jNOAVTX8PPE",
+        duration: 108,
+        dateAired: "05/20/2016",
+        ratingImdb: 6.1,
+        description:
+            "A 15-year-old boy steals a bag of drug money and runs off with the girl he loves. As her violent and corrupt police officer father hunts them down, they embark on a journey that will change their lives forever.",
+        photoSrc: "http://localhost:4000/images/movies/AlowOYyprAAq85PPtE9LvoiYT1b.jpg",
+        photoSrcProd:
+            "https://movielandia-avenger22s-projects.vercel.app/images/movies/AlowOYyprAAq85PPtE9LvoiYT1b.jpg",
+    },
+];
