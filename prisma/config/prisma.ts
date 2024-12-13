6d8f70d623390f3e5a66425ef9eb9b1196be13d0
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-12-13 11:17:50 +0100

    fixed the follow unfollow in new pages

--- prisma/config/prisma.ts
+++ prisma/config/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
     globalForPrisma.prisma ||
@@ -9,4 +9,5 @@ export const prisma =
         errorFormat: "pretty",
     });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.VERCEL_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
