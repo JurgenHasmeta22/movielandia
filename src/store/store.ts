a71b84127b4d5ddbb3d165e3cad385862cd9ff6b
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2025-01-01 15:43:24 +0100

    fixed few rules instructions

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,10 +9,8 @@ export const useStore = create<AppStoreState>()(
         (set): AppStoreState => ({
             isOpenSidebarAdmin: true,
             setIsOpenSidebarAdmin: (data) => set({ isOpenSidebarAdmin: data }),
-
             isDrawerOpen: false,
             setIsDrawerOpen: (data) => set({ isDrawerOpen: data }),
-
             isEditModeReview: false,
             setIsEditModeReview: (data) => set({ isEditModeReview: data }),
         }),
--- src/types/IStore.ts
+++ src/types/IStore.ts
@@ -1,10 +1,8 @@
 export default interface AppStoreState {
     isOpenSidebarAdmin: boolean;
     setIsOpenSidebarAdmin: (data: boolean) => void;
-
     isDrawerOpen: boolean;
     setIsDrawerOpen: (data: boolean) => void;
-
     isEditModeReview: boolean;
     setIsEditModeReview: (data: boolean) => void;
 }
