e742a829943077ac77c10cd02e97ef6d12e7ab55
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2025-01-30 10:11:55 +0100

    Version update

--- prisma/config/prisma.ts
+++ prisma/config/prisma.ts
@@ -10,4 +10,3 @@ export const prisma =
     });
 
 if (process.env.VERCEL_ENV !== "production") globalForPrisma.prisma = prisma;
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
