d4f51e668b101fe8763cf6a7d8aa73d014f76be5
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2025-02-18 09:39:16 +0100

    prisma optimize production fix

--- prisma/config/prisma.ts
+++ prisma/config/prisma.ts
@@ -8,6 +8,6 @@ export const prisma =
     new PrismaClient({
         log: ["query", "info", "warn", "error"],
         errorFormat: "pretty",
-    }).$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY!, enable: process.env.ENVIRONMENT === 'development', }));
+    }).$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY!, }));
 
 if (process.env.VERCEL_ENV !== "production") globalForPrisma.prisma = prisma;
