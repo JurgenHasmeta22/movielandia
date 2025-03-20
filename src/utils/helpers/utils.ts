70c92f57a7d8540b5262a34728789afe2dac8768
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2025-03-20 14:33:44 +0100

    fix error build

--- src/utils/helpers/utils.ts
+++ src/utils/helpers/utils.ts
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
-import ImageKit from "imagekit";
+// import ImageKit from "imagekit";
 
 export const IS_BROWSER = typeof window !== "undefined" && typeof window?.document !== "undefined";
 
@@ -10,8 +10,8 @@ export const formatDate = (date: Date) => {
     return format(date, "dd MMMM, yyyy");
 };
 
-export const imagekit = new ImageKit({
-    publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
-    privateKey: process.env.PRIVATE_KEY!,
-    urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
-});
+// export const imagekit = new ImageKit({
+//     publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+//     privateKey: process.env.PRIVATE_KEY!,
+//     urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+// });
