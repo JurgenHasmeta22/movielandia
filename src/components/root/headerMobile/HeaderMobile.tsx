0d80d1d27087ee66097356dca5a1737ad3f6cc1c
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2025-03-20 18:08:47 +0100

    header mobile rewowrk

--- src/components/root/headerMobile/HeaderMobile.tsx
+++ src/components/root/headerMobile/HeaderMobile.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CloseOutlined } from "@mui/icons-material";
-import { Box, SwipeableDrawer, IconButton, useMediaQuery, useTheme, Button, Divider } from "@mui/material";
+import { Box, SwipeableDrawer, IconButton, useMediaQuery, useTheme } from "@mui/material";
 import { useStore } from "@/store/store";
 import { useEffect } from "react";
 import { Session } from "next-auth";
@@ -12,7 +12,6 @@ import SearchField from "../searchField/SearchField";
 import { Genre } from "@prisma/client";
 import EmailIcon from "@mui/icons-material/Email";
 import NotificationMenu from "../notificationMenu/NotificationMenu";
-import Image from "next/image";
 import Link from "next/link";
 
 interface IHeaderMobileProps {
@@ -64,129 +63,54 @@ export default function HeaderMobile({
                 },
             }}
         >
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    height: "100%",
-                    overflow: "hidden",
-                }}
-            >
-                <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        p: 2,
-                        borderBottom: 1,
-                        borderColor: "divider",
-                    }}
-                >
-                    <Link href="/" onClick={() => setIsDrawerOpen(false)}>
-                        <Image
-                            src="/icons/movielandia24-logo.png"
-                            alt="MovieLandia24"
-                            height={55}
-                            width={170}
-                            priority
-                        />
-                    </Link>
+            <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+                <Box sx={{ p: 1, borderBottom: 1, borderColor: "divider" }}>
                     <IconButton
                         onClick={() => setIsDrawerOpen(false)}
                         sx={{
                             color: theme.vars.palette.primary.main,
-                            "&:hover": {
-                                color: theme.vars.palette.green.main,
-                            },
+                            "&:hover": { color: theme.vars.palette.green.main },
                         }}
                     >
                         <CloseOutlined />
                     </IconButton>
                 </Box>
-
-                <Box
-                    sx={{
-                        p: 2,
-                        width: "100%",
-                    }}
-                >
-                    <SearchField
-                        onClose={() => setIsDrawerOpen(false)}
-                        isMobile={true}
-                        customStyles={{
-                            width: "100%",
-                        }}
-                    />
-                </Box>
-
-                <Box
-                    sx={{
-                        flex: 1,
-                        overflowY: "auto",
-                        px: 2,
-                        py: 1,
-                    }}
-                >
-                    <HeaderLinks genres={genres} />
-                </Box>
-
-                <Box
-                    sx={{
-                        p: 2,
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: 2,
-                    }}
-                >
+                <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
+                    <Box sx={{ width: "100%" }}>
+                        <HeaderLinks genres={genres} />
+                    </Box>
                     {session?.user && (
-                        <Box
-                            sx={{
-                                display: "flex",
-                                gap: 1,
-                                justifyContent: "flex-start",
-                                alignItems: "center",
-                            }}
-                        >
-                            <Box sx={{ display: "flex", gap: 1 }}>
-                                <Link
-                                    href="/messages"
-                                    onClick={() => setIsDrawerOpen(false)}
-                                    style={{ textDecoration: "none" }}
-                                >
-                                    <IconButton
-                                        size="small"
-                                        sx={{
-                                            color: theme.vars.palette.primary.main,
-                                            "&:hover": {
-                                                color: theme.vars.palette.green.main,
-                                            },
-                                        }}
+                        <Box sx={{ mt: "auto", borderTop: 1, borderColor: "divider" }}>
+                            <Box sx={{ p: 1 }}>
+                                <SearchField
+                                    onClose={() => setIsDrawerOpen(false)}
+                                    isMobile={true}
+                                    customStyles={{ width: "100%", mb: 1 }}
+                                />
+                                <Box sx={{ display: "flex", gap: 0.5 }}>
+                                    <Link
+                                        href="/messages"
+                                        onClick={() => setIsDrawerOpen(false)}
+                                        style={{ textDecoration: "none" }}
                                     >
-                                        <EmailIcon />
-                                    </IconButton>
-                                </Link>
-                                <NotificationMenu session={session} />
+                                        <IconButton size="small" sx={{ color: theme.vars.palette.primary.main }}>
+                                            <EmailIcon />
+                                        </IconButton>
+                                    </Link>
+                                    <NotificationMenu session={session} />
+                                    <ThemeToggleButton />
+                                    <AuthButtons
+                                        session={session}
+                                        userName={userName}
+                                        anchorElProfile={anchorElProfile}
+                                        closeMenuProfile={closeMenuProfile}
+                                        openMenuProfile={openMenuProfile}
+                                        handleSignOut={handleSignOut}
+                                    />
+                                </Box>
                             </Box>
                         </Box>
                     )}
-
-                    <Box
-                        sx={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                        }}
-                    >
-                        <ThemeToggleButton />
-                        <AuthButtons
-                            session={session}
-                            userName={userName}
-                            anchorElProfile={anchorElProfile}
-                            closeMenuProfile={closeMenuProfile}
-                            openMenuProfile={openMenuProfile}
-                            handleSignOut={handleSignOut}
-                        />
-                    </Box>
                 </Box>
             </Box>
         </SwipeableDrawer>
