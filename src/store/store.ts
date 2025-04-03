e9920ca4040908f94c81d42dec3cea89f74fbd49
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2025-04-03 13:52:17 +0200

    format stuff

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,8 +9,10 @@ export const useStore = create<AppStoreState>()(
         (set): AppStoreState => ({
             isOpenSidebarAdmin: true,
             setIsOpenSidebarAdmin: (data) => set({ isOpenSidebarAdmin: data }),
+
             isDrawerOpen: false,
             setIsDrawerOpen: (data) => set({ isDrawerOpen: data }),
+            
             isEditModeReview: false,
             setIsEditModeReview: (data) => set({ isEditModeReview: data }),
         }),
--- src/types/IStore.ts
+++ src/types/IStore.ts
@@ -1,8 +1,10 @@
 export default interface AppStoreState {
     isOpenSidebarAdmin: boolean;
     setIsOpenSidebarAdmin: (data: boolean) => void;
+
     isDrawerOpen: boolean;
     setIsDrawerOpen: (data: boolean) => void;
+    
     isEditModeReview: boolean;
     setIsEditModeReview: (data: boolean) => void;
 }
