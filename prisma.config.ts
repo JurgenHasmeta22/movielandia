8a2eda9e23f57f2de6854ded02d9e9c8494350c0
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2026-01-28 17:33:45 +0100

    fix: Prisma bad import

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,10 +2,9 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-	schema: "prisma/schema/main.prisma",
+	schema: "prisma/schema",
 	datasource: {
 		url: env("POSTGRES_PRISMA_URL"),
-		// shadowDatabaseUrl: env("POSTGRES_URL_NON_POOLING"),
 	},
 	migrations: {
 		path: "prisma/schema/migrations",
